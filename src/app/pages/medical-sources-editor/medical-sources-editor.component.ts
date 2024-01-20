dd5a27b39712033415baee5609c3a0be01970d05
Jason Kulatunga <jason@thesparktree.com>
2024-01-20 11:11:26 -0800

    strip base64 prefix before sending to server.

--- src/app/pages/medical-sources-editor/medical-sources-editor.component.ts
+++ src/app/pages/medical-sources-editor/medical-sources-editor.component.ts
@@ -201,7 +201,8 @@ export class MedicalSourcesEditorComponent implements OnInit {
       let reader = new FileReader();
       reader.onloadend = () => {
         // use a regex to remove data url part
-        this.logo_file.get('file_content').setValue(reader.result as string)
+        const base64String = (reader.result as string).replace('data:', '').replace(/^.+,/, '');
+        this.logo_file.get('file_content').setValue(base64String)
       };
       reader.readAsDataURL(fileInput.files[0]);
       this.logo_file.get('file_name').setValue(fileInput.files[0].name)
