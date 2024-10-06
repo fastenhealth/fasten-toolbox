3473d1a445402df0981f2371da146afee520713b
Jason Kulatunga <jason@thesparktree.com>
2024-10-05 23:01:59 -0400

    adding callback logic.

--- src/app/pages/medical-records-export-callback/medical-records-export-callback.component.ts
+++ src/app/pages/medical-records-export-callback/medical-records-export-callback.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {ToolboxService} from '../../services/toolbox.service';
 
 @Component({
   selector: 'app-medical-records-export-callback',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicalRecordsExportCallbackComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activatedRoute : ActivatedRoute,
+    private toolboxService: ToolboxService,
+  ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(values => {
+      this.toolboxService.recordsExportCallback(values).subscribe((res) => {
+        console.log(res)
+
+        //make calls to the download endpoint every 10 seconds, until we get an error or a success payload
+        let cancel = setInterval(() => {
+          this.toolboxService.recordsExportDownload().subscribe((res) => {
+            console.log(res)
+            if(res.data.status == 'success' || res.data.status == 'failed'){
+              clearInterval(cancel)
+            }
+
+            //TODO: if success, present the content and download link to the user
+
+            //TODO: if error present the error to the user
+          }, (err) => {
+            clearInterval(cancel)
+          })
+        }, 10000)
+
+      })
+    })
   }
 
 }
--- src/app/services/toolbox.service.ts
+++ src/app/services/toolbox.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {ResponseWrapper} from '../models/response-wrapper';
 import {map} from 'rxjs/operators';
+import {Params} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToolboxService {
 
+  platform_url = 'https://api.platform.fastenhealth.com/v1'
+
   constructor(private _httpClient: HttpClient) { }
 
   catalogEditor(submission: any): Observable<any> {
-    return this._httpClient.post<any>(`https://api.platform.fastenhealth.com/v1/catalog/editor`, submission)
+    return this._httpClient.post<any>(`${this.platform_url}/catalog/editor`, submission)
       .pipe(
         map((response: ResponseWrapper) => {
           console.log("BUNDLE RESPONSE", response)
@@ -21,4 +24,35 @@ export class ToolboxService {
         })
       );
   }
+
+  //returns authenticated or error
+  recordsExportCallback(queryStringParams: Params ): Observable<boolean> {
+    return this._httpClient.get<any>(`${this.platform_url}/records/export/callback`,
+      {
+        params: queryStringParams,
+        withCredentials: true
+      })
+      .pipe(
+        map((response: ResponseWrapper) => {
+          console.log("RECORDS EXPORT CALLBACK RESPONSE", response)
+          // @ts-ignore
+          return true
+        })
+      );
+  }
+
+  recordsExportDownload(): Observable<any> {
+    return this._httpClient.get<any>(`${this.platform_url}/records/export/download`,
+      {
+        withCredentials: true
+      })
+      .pipe(
+        map((response: ResponseWrapper) => {
+          console.log("RECORDS EXPORT DOWNLOAD RESPONSE", response)
+          // @ts-ignore
+          return response.data
+        })
+      );
+  }
+
 }
