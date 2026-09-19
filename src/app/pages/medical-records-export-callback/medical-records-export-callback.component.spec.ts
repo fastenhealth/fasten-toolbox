0e83d5923c39bf0d48522dc2109191d501af3d84
Jason Kulatunga <jason@fastenhealth.com>
2026-09-19 13:46:18 -0400

    update: revamp medical records export callback page layout and design for improved user experience

--- src/app/pages/medical-records-export-callback/medical-records-export-callback.component.spec.ts
+++ src/app/pages/medical-records-export-callback/medical-records-export-callback.component.spec.ts
@@ -1,5 +1,11 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { of } from 'rxjs';
 
+import { ToolboxService } from '../../services/toolbox.service';
 import { MedicalRecordsExportCallbackComponent } from './medical-records-export-callback.component';
 
 describe('MedicalRecordsExportCallbackComponent', () => {
@@ -8,7 +14,23 @@ describe('MedicalRecordsExportCallbackComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MedicalRecordsExportCallbackComponent ]
+      declarations: [ MedicalRecordsExportCallbackComponent ],
+      imports: [ RouterTestingModule ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParams: of({
+              error: 'authorization_failed',
+              error_description: 'The request could not be completed.',
+              request_id: 'request-one',
+            })
+          }
+        },
+        { provide: ToolboxService, useValue: jasmine.createSpyObj('ToolboxService', [ 'recordsExportCallback' ]) },
+        { provide: NgbModal, useValue: jasmine.createSpyObj('NgbModal', [ 'open' ]) },
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ],
     })
     .compileComponents();
 
@@ -19,5 +41,8 @@ describe('MedicalRecordsExportCallbackComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('.callback-panel')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('.phone-frame')).toBeNull();
+    expect(fixture.nativeElement.querySelector('.error-card')).toBeTruthy();
   });
 });
